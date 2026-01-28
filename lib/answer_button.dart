6fcfd92a8fce9d3e258fe0cc344f58efb56a1baf
sayuj <sayuj.sehgal@gmail.com>
2026-01-28 09:50:52 +0530

    dynamically render quiz questions and options

---
+++ lib/answer_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    required this.answer,
+    required this.onSelected,
+    super.key,
+  });
+
+  final String answer;
+  final void Function() onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onSelected,
+      style: ElevatedButton.styleFrom(foregroundColor: Colors.blue),
+      child: Text(answer),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'answer_button.dart';
+import 'package:first_project/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -8,8 +10,33 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  int currentQuestionIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              currentQuestion.question,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                height: 1.2,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          ...currentQuestion.options.map((option) {
+            return AnswerButton(answer: option, onSelected: () {});
+          }),
+        ],
+      ),
+    );
   }
 }
